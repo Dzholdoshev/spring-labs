2c2ab06546155893453be81f3a9a1877f5975f4e
marklen <marklen86@gmail.com>
2022-09-28 23:16:43 -0500

    Project Employee registration

--- employee-registration-project/src/main/java/com/cydeo/controller/EmployeeController.java
+++ employee-registration-project/src/main/java/com/cydeo/controller/EmployeeController.java
@@ -2,14 +2,27 @@ package com.cydeo.controller;
 
 import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Employee;
+import com.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
 
     @GetMapping("/register")
     public String createEmployee(Model model) {
@@ -18,6 +31,25 @@ public class EmployeeController {
         model.addAttribute("stateList", DataGenerator.getAllStates());
 
         return "employee/employee-create";
+    }
+
+
+    @PostMapping("/insert")
+    public String insertEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult,Model model) {
+
+       if(bindingResult.hasErrors()){
+           model.addAttribute("stateList", DataGenerator.getAllStates());
+           return "employee/employee-create";
+       }
+
+        employeeService.saveEmployee(employee);
+        return "redirect:/employee/list"; // With redirect, we are using endpoints
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        model.addAttribute("employeeList", employeeService.readAllEmployee());
+    return "employee/employee-list"; // Without redirect, we are using html file paths
     }
 
 
